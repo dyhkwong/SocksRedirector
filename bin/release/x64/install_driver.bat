dbc8e637b064ea2c322d5b0fcdd9119f47dbdcb4
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2022-09-30 21:50:22 +0800

    Initial commit
    imported from "nfsdk-demo-pf-1.6.4.9.zip"

---
+++ bin/release/x64/install_driver.bat
@@ -0,0 +1,11 @@
+cd /d %~dp0
+
+rem Installing the network hooking driver build for 64-bit systems
+
+rem Copy the driver to system folder
+copy netfilter2.sys %windir%\system32\drivers
+
+rem Register the driver
+nfregdrv.exe netfilter2
+
+pause
---
+++ bin/release/x64/uninstall_driver.bat
@@ -0,0 +1,14 @@
+cd /d %~dp0
+
+rem Uninstall the network hooking driver
+
+rem Try to unload the driver
+sc stop netfilter2
+
+rem Unregister the driver
+nfregdrv.exe -u netfilter2
+
+rem Delete driver file
+del %windir%\system32\drivers\netfilter2.sys
+
+pause
